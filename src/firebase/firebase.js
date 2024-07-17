479b87a1ed495099ac29d0c59220b21894fdf9d1
Shreyansh7A <shreyansh7a@gmail.com>
2024-07-17 16:07:29 -0700

    addid config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,17 +2,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import {getAuth} from "firebase/auth";
+const config = require('../firebase_config.json')
+
+const firebaseConfig = {
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId
+};
+
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA6uA7tx-gfeluNPuy1xIS8bX4yPI316d4",
-  authDomain: "blockdata-4e11f.firebaseapp.com",
-  projectId: "blockdata-4e11f",
-  storageBucket: "blockdata-4e11f.appspot.com",
-  messagingSenderId: "302074203261",
-  appId: "1:302074203261:web:b1d717a93cefc432e5b514",
-  measurementId: "G-MFEVL5ETJF"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
